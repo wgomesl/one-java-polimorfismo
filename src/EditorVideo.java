c6bee5253dd8004ab9d242764bdc2f41938d09cb
wesley <wesley.gomes222@hotmail.com>
2023-02-22 15:01:11 -0300

    aplicando polimorfismo

---
+++ src/EditorVideo.java
@@ -0,0 +1,11 @@
+
+public class EditorVideo extends Funcionario {
+	
+	public double getBonificacao() {
+		
+		System.out.println("Chamando o método de bonificação do EDITOR DE VIDEO");
+		return super.getBonificacao() + 100;
+	}
+	
+	
+}
--- src/Gerente.java
+++ src/Gerente.java
@@ -17,6 +17,7 @@ public class Gerente extends Funcionario {
 	
 	//primeiro método
 	public double getBonificacao() {
+		System.out.println("Chamando o método de bonificação do GERENTE");
 		return super.getBonificacao() + super.getSalario();
 	}
 	
